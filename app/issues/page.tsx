3e6cb2695d9a80dccffd08ef5edef3a00ed2de7b
shenith_s <shenith@surge.global>
2024-04-20 12:42:41 +0530

    add new issue page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,13 @@
 import { Button } from "@radix-ui/themes";
+import Link from "next/link";
 import React from "react";
 
 const IssuesPage = () => {
   return (
     <div>
-      <Button>Add issue</Button>
+      <Button>
+        <Link href={"/issues/new"}>Add new issue</Link>
+      </Button>
     </div>
   );
 };

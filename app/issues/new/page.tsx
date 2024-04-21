aa1da2b3f7717d1fb49e5d2cad2f4c5ca5063c15
shenith_s <shenith@surge.global>
2024-04-21 21:21:17 +0530

    add form handlers and handle errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,12 +1,52 @@
-import { Button, TextArea, TextField } from "@radix-ui/themes";
-import React from "react";
+"use client";
+import { Button, Callout, TextArea, TextField } from "@radix-ui/themes";
+import React, { useState } from "react";
+import { useForm, SubmitHandler, Controller } from "react-hook-form";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+interface AddIssueForm {
+  title: string;
+  description: string;
+}
 
 const NewIssuePage = () => {
+  const [error, setError] = useState("");
+  const router = useRouter();
+  const { register, handleSubmit, control } = useForm<AddIssueForm>();
+  const onSubmit: SubmitHandler<AddIssueForm> = async (data) => {
+    try {
+      await axios.post("/api/issues", data);
+      router.push("/issues");
+    } catch (error) {
+      setError("Unexpected error occered");
+    }
+  };
   return (
-    <div className=" max-w-lg space-y-3">
-      <TextField.Root placeholder="Issue title"></TextField.Root>
-      <TextArea placeholder="Description" />
-      <Button>Submit issue</Button>
+    <div className="max-w-lg ">
+      {error && (
+        <Callout.Root className=" mb-5" color="red">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+
+      <form onSubmit={handleSubmit(onSubmit)} className=" space-y-3">
+        <TextField.Root
+          placeholder="Issue title"
+          {...register("title")}
+        ></TextField.Root>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE {...field} placeholder="Description" />
+          )}
+        />
+
+        <Button type="submit">Submit issue</Button>
+      </form>
     </div>
   );
 };
